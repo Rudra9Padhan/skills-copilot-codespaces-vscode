efe5428d308f9905dcae9c73027f5c0ac75b9dac
Rudranarayan Padhan <161827362+Rudra9Padhan@users.noreply.github.com>
2025-03-23 06:41:35 +0000

    third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,19 @@
 // Create Web Server
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the Web Server!');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
 
